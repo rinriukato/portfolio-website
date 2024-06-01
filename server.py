72816173bb41772195e8d4884960064bff3bd76c
Isaiah N. <rinriukato@gmail.com>
2024-06-01 13:23:22 -0400

    create multiple routes for multiple pages for the future

--- server.py
+++ server.py
@@ -3,9 +3,25 @@ from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route('/skills', methods=['GET'])
+def skills():
+    return render_template('skills.html')
+
+
+@app.route('/about-me', methods=['GET'])
+def about_me():
+    return render_template('about-me.html')
+
+
+@app.route("/featured-projects", methods=['GET'])
+def projects():
+    return render_template('projects.html')
 
 
 if __name__ == "__main__":
