47184c9ab7e5a05109f62c278f22f3fa4edcb7c3
Isaiah N. <rinriukato@gmail.com>
2024-06-16 17:10:21 -0400

    made page responsive. incorrect usage of media size caused hard limit on scaling of project cards

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,10 +2,9 @@ const punctuation_timer = 145;
 const space_timer = 17;
 const letter_timer = 10;
 
-
 const dialog = [
     "Hi! My name is Isaiah, and I'm a software developer. You can check out some of my work by scrolling down below, or you can see my skills by clicking on this textbox!",
-    "I typically use Python and Javascript with a bit of C#, HTML, and CSS. I do have some knowledge with C++ and Java too",
+    "I typically use Python and Javascript with a bit of C#, HTML, and CSS. I do have some knowledge with C++ and Java too.",
     "Some of the software and frameworks that I've used are the Godot and Unity game engines, Git, Docker, MongoDB, Flask, Bootstrap, and Postman!"
 
 ]
@@ -13,13 +12,27 @@ const dialog = [
 var dialogIndex = 0
 var can_click = true
 
+const dialogFinishedEvent = new Event("dialogFinished")
+const dialogStartEvent = new Event('dialogStart')
+const arrow = document.querySelector("#arrow-right")
+
+document.addEventListener('dialogFinished', e => {
+    arrow.style.opacity = 1;
+    console.log('in')
+})
+
+document.addEventListener('dialogStart', e => {
+    arrow.style.opacity = 0;
+    console.log('out')
+})
+
 // On start up
 document.addEventListener('DOMContentLoaded', function(e) {
     const text = document.querySelector(".dialog-text");
+    document.dispatchEvent(dialogStartEvent)
     printLetterByLetter(text, dialog[dialogIndex++], 10);
 
 });
-
 
 const clip = document.querySelectorAll(".hover-to-play");
 for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", function (e) { clip[i].play();
@@ -28,9 +41,8 @@ for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", f
 
 const textBox = document.querySelector(".dialog-box");
 textBox.addEventListener("click", function(e) {
-    const text = document.querySelector(".dialog-text");
-    //text.innerHTML = dialog[dialogIndex++]
-
+   const text = document.querySelector(".dialog-text");
+   document.dispatchEvent(dialogStartEvent)
    printLetterByLetter(text, dialog[dialogIndex++], 10);
 
     if (dialogIndex >= dialog.length) {
@@ -72,4 +84,5 @@ async function printLetterByLetter(destination, message, speed){
     }
 
     can_click = true
+    document.dispatchEvent(dialogFinishedEvent)
 }
